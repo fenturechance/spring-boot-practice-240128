5b69a4eee897fbf06a0b513441ac5315db072392
fenturechance <fenturechance@gmail.com>
2024-01-29 00:01:28 +0800

    AppConfig

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,17 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         //TestInterface testInterface = new TestClass1();
         //testInterface.call();
-        TestInterface testInterface = applicationContext.getBean("TestInterface", TestInterface.class);
-        testInterface.call();
+        //TestInterface testInterface = applicationContext.getBean("TestInterface", TestInterface.class);
+        //testInterface.call();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        TestClass1 testClass1 = context.getBean(TestClass1.class);
+        System.out.println(testClass1);
         SpringApplication.run(DemoApplication.class, args);
     }
 
--- src/main/java/com/example/demo/TestClass1.java
+++ src/main/java/com/example/demo/TestClass1.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 public class TestClass1 implements TestInterface{
     private String testStringProp;
+    public TestClass1() {
+
+    }
 
     public TestClass1(String testStringProp) {
         this.testStringProp = testStringProp;
