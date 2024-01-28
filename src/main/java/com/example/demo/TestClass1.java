90a56657251112a2d340631f85eff91e9cc26d4a
fenturechance <fenturechance@gmail.com>
2024-01-28 23:42:00 +0800

    constructor-arg

--- src/main/java/com/example/demo/TestClass1.java
+++ src/main/java/com/example/demo/TestClass1.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 public class TestClass1 implements TestInterface{
+    private String testStringProp;
+
+    public TestClass1(String testStringProp) {
+        this.testStringProp = testStringProp;
+    }
     @Override public void call() {
-        System.out.println("1 call");
+        System.out.println("1 call " + this.testStringProp);
     }
 }
