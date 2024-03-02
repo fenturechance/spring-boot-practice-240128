9319705f4c6c7eed7e5024e01ec98a995758ac43
fenturechance <fenturechance@gmail.com>
2024-03-03 00:21:12 +0800

    return new WebMvcConfigurer() {}

--- src/main/java/com/example/demo/AppConfig3.java
+++ src/main/java/com/example/demo/AppConfig3.java
@@ -1,17 +1,32 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-public class AppConfig3 implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        WebMvcConfigurer.super.addResourceHandlers(registry);
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/a/", "classpath:/b/").setCacheControl(CacheControl.maxAge(7200, TimeUnit.MINUTES));
+public class AppConfig3 /*implements WebMvcConfigurer   */ {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        WebMvcConfigurer.super.addResourceHandlers(registry);
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/a/", "classpath:/b/").setCacheControl(CacheControl.maxAge(7200, TimeUnit.MINUTES));
+//    }
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/a/", "classpath:/b/").setCacheControl(CacheControl.maxAge(7200, TimeUnit.MINUTES));
+            }
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+//                registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
+            }
+        };
     }
 }
